724a0ab0dc379426be163fb3315c066f852de09a
ferreol <ferreol.soulez@free.fr>
2026-03-18 09:14:39 +0100

    fix image read/write bugs

--- src/images.jl
+++ src/images.jl
@@ -183,7 +183,7 @@ function read!(arr::DenseArray{T,L},
         # because it is not possible to guess one given the other and the size of the array
         # to read as it may have fewer dimensions than the image.
         if step === nothing
-            ipix = NTuple(i -> one(Clong), Val(N))::NTuple{N,Clong}
+            ipix = ntuple(i -> one(Clong), Val(N))::NTuple{N,Clong}
         else
             ipix = as(NTuple{N,Clong}, step)
         end
@@ -211,7 +211,7 @@ function read!(arr::DenseArray{T,L},
                     "image extension and output array have differente sizes"))
                 fpix = 1
             else first === nothing
-                fpix = Int(fpix, len + 1 - last)::Int
+                fpix = Int(last + 1 - len)::Int
             end
         else
             last === nothing || last + 1 - first == len || throw(DimensionMismatch(
@@ -222,11 +222,11 @@ function read!(arr::DenseArray{T,L},
         if null isa DenseArray{Bool,L}
             axes(null) == axes(arr) || error("incompatible array axes")
             if len > 0
-                # NOTE: `GC.@protect null` is not needed here.
-                _null = logical_pointer(null)
-                check(CFITSIO.fits_read_imgnull(
-                    hdu, type, fpix, len, _null, arr, anynul, Ref{Cint}(0)))
-                fix_logicals!(null)
+                GC.@preserve null begin
+                    _null = Cstring(Base.unsafe_convert(Ptr{Cchar}, pointer(null)))
+                    check(CFITSIO.fits_read_imgnull(
+                        hdu, type, fpix, len, arr, _null, anynul, Ref{Cint}(0)))
+                end
             end
         elseif len > 0
             _null = (null === nothing ? C_NULL : null)
@@ -429,7 +429,7 @@ function write(hdu::FitsImageHDU{<:Any,N},
                     "image extension and input array have different sizes"))
                 fpix = 1
             else first === nothing
-                fpix = Int(fpix, len + 1 - last)::Int
+                fpix = Int(last + 1 - len)::Int
             end
         else
             last === nothing || last + 1 - first == len || throw(DimensionMismatch(
